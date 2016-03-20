f10eae04134116a5a725d170ee89c5f36f69b37a
Alan Chow <hello@alanchow.ca>
2016-03-20 15:12:26 -0700

    restore proper main.jsx file to src folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, Redirect } from 'react-router';
+import { Router, Route, browserHistory, Redirect } from 'react-router';
+
+import Questions from './components/questions.jsx';
+import Welcome from './components/welcome-screen.jsx';
+import NotFound from './components/404.jsx';
+import Rejected from './components/failure.jsx';
+import Accepted from './components/success.jsx';
 
 var MarsApp = React.createClass({
-  getInitialState: function() {
-    return {
-      minutesElapsed: 5,
-      secondsElapsed: Math.floor(this.minutesElapsed % 60)
-    }
-  },
-  resetTimer: function() {
-    clearInterval(this.interval);
-    this.setState({ secondsElapsed: 0 });
-    this.start();
-  },
-  tick: function() {
-    this.setState({secondsElapsed: this.state.secondsElapsed - 1});
-    if (this.state.secondsElapsed <= 0) {
-      clearInterval(this.interval);
-    }
-  },
-  start: function() {
-    this.interval = setInterval(this.tick, 100);
-  },
-  componentDidMount: function() {
-    setTimeout(this.start, this.props.timeout);
-  },
-  componentWillUnmount: function() {
-    clearInterval(this.interval);
-  },
+
   render: function() {
     return (
-    <div>
-      <div className="wrapper">
-        <div className="header">
-          <h1>Mars</h1>
-          <div className="shuttle">
-            <i className="fa fa-space-shuttle"></i>
-          </div>
-        </div>
-        <div className="test-area">
-          <button className="eval-start">Begin Evaluation</button>
-        </div>
-      </div> {/*end wrapper*/}
-        <div className="timer">
-          <p>{this.state.minutesElapsed}:{this.state.secondsElapsed < 10 ? '0'+this.state.secondsElapsed : this.state.secondsElapsed}</p>
-        </div>
-      </div>
-    )
+      <Router history={browserHistory}>
+        <Redirect from='/' to='/welcome' />
+        <Route path='/welcome' component={Welcome} />
+        <Route path='/mars-test' component={Questions} />
+        <Route path='/success' component={Accepted} />
+        <Route path='/failure' component={Rejected} />
+        <Route path="*" component={NotFound} />
+      </Router>
+    );
   }
+
 });
+
 
 ReactDOM.render(<MarsApp />, document.querySelector('#mars-mount'));
